61c269b6aa1410bee78282f134b86c73739f6ba0
yneli <Mr.RadReIII@yandex.ru>
2023-04-21 15:22:53 +0300

    port

--- src/components/ChatPanelMsg/ChatPanelMsg.tsx
+++ src/components/ChatPanelMsg/ChatPanelMsg.tsx
@@ -7,8 +7,8 @@ import { FriendMsg } from "../ChatPanel/items/FriendMsg";
 import Image from "next/image";
 import { useAppSelector } from "@/redux/hook";
 
-const socket = io("http://localhost:4000");
-// const socket = io("https://chat2-0-back.onrender.com/");
+// const socket = io("http://localhost:4000");
+const socket = io("https://chat2-0-back.onrender.com/");
 export const ChatPanelMsg = () => {
 
   const [ message, setMessage ] = React.useState("");
